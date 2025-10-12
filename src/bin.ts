d446ef340af5577ad1e14206a28b70482ae48d6f
gomi <gxy880520@qq.com>
2025-10-12 09:04:57 +0800

    chore: add main function

---
+++ src/bin.ts
@@ -0,0 +1,16 @@
+#!/usr/bin/env bun
+
+import { BunParallelCLI } from "./cli";
+
+async function main(): Promise<void> {
+  const cli = new BunParallelCLI();
+  const exitCode = await cli.run(process.argv.slice(2));
+  process.exit(exitCode);
+}
+
+if (import.meta.main) {
+  main().catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
+}
